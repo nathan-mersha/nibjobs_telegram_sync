5e40606588051efaf7c2e2e244fbaa5741be3fec
michaelandom03 <michaelandom03@gmail.com>
2021-12-02 11:29:39 +0300

    added 8 job channels and fixed bugs

--- lib/job_channels/jobs_in_ethiopia.py
+++ lib/job_channels/jobs_in_ethiopia.py
@@ -53,8 +53,8 @@ class JobInEthiopia(JobChannelMethods):
     def extract_job_description(self, message: str):
         message=self.deEmojify(message)
         return message.lower() \
-            .replace("https://ethiopage.com/jobs", "") \
-            .replace("ሌሎች የስራ ማስታወቂያዎችን ዌብሳይቱ ላይ ማግኘት ትችላላችሁ", "") \
+            .replace("https://t.me/jobs_in_inter", "") \
+            .replace("for more jobs  join", "") \
             .replace("ሌሎች ስራ መረጃዎችን ለማግኘት ይህንን ሊንክ ይጫኑት", "") \
             .replace("https://t.me/joinchat/AAAAAErPpgYpd5Yl8CBg8g", "") \
             .replace("#other", "") \
